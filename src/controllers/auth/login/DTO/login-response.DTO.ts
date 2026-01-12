82f654f7f1685355660e069c02dcaf54ffe4119b
Felipe Oliveira Souza <flipeaz342@gmail.com>
2026-01-12 13:11:15 -0400

    refactor: simplify LoginResponseDTO constructor by using parameter properties

--- src/controllers/auth/login/DTO/login-response.DTO.ts
+++ src/controllers/auth/login/DTO/login-response.DTO.ts
@@ -1,21 +1,9 @@
 export class LoginResponseDTO {
-  access_token: string;
-  refresh_token: string;
-  user_id: number;
-  username: string;
-  expires_at: Date;
-
   constructor(
-    access_token: string,
-    refresh_token: string,
-    user_id: number,
-    username: string,
-    expires_at: Date,
-  ) {
-    this.access_token = access_token;
-    this.refresh_token = refresh_token;
-    this.user_id = user_id;
-    this.username = username;
-    this.expires_at = expires_at;
-  }
+    public readonly access_token: string,
+    public readonly refresh_token: string,
+    public readonly user_id: number,
+    public readonly username: string,
+    public readonly expires_at: Date,
+  ) {}
 }
